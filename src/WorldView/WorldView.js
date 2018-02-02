8bb2ae2b78f3efa9cfe997c879ddb1f6a2bf92fa
Timothy Lanahan <timothy.lanahan@fts-intl.local>
2018-02-01 21:37:20 -0500

    add mock satellites and incorporate map function into WorldView JSX

--- src/WorldView/WorldView.js
+++ src/WorldView/WorldView.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 import AccordionTitleCheckbox from '../common/AccordionTitleCheckbox';
 import IconWithIndex from '../common/IconWithIndex';
+import satellites from '../api/mockSatellites';
 
 import './WorldView.css';
 
@@ -53,97 +54,23 @@ export default class WorldView extends React.Component {
       <Grid className='mgn-0' columns={16}>
         <Grid.Column className='mgn-0 pd-0' stretched={true} width={3}>
             <Accordion as={Menu} className='bdr-rd-0' width='thin' fluid vertical>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={0}>
-                    <Checkbox label='Sat1' name='Sat1' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={0} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(0) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={1}>
-                    <Checkbox label='Sat2' name='Sat2' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={1} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(1) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={2}>
-                    <Checkbox label='Sat4' name='Sat4' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={2} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(2) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={3}>
-                    <Checkbox label='Sat6' name='Sat6' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={3} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(3) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={4}>
-                    <Checkbox label='Sat8' name='Sat8' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={4} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(4) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={5}>
-                    <Checkbox label='Sat9' name='Sat9' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={5} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(5) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
-              <Menu.Item>
-                <AccordionTitleCheckbox index={6}>
-                    <Checkbox label='Sat10' name='Sat10' onClick={this.getOrbitData}/>
-                    <IconWithIndex index={6} name='dropdown' onClick={this.toggleAccordionContent}/>
-                </AccordionTitleCheckbox>
-                <Accordion.Content active={this.state.activePanes.indexOf(6) > -1}>
-                  <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
-                    <Checkbox label='Ant1' />
-                    <Checkbox label='Ant2' />
-                    <Checkbox label='Ant3' />
-                  </div>
-                </Accordion.Content>
-              </Menu.Item>
+              {
+                satellites.map((s, idx) => {
+                  return (
+                    <Menu.Item>
+                      <AccordionTitleCheckbox index={idx}>
+                        <Checkbox label={s.name} name={s.name} onClick={this.getOrbitData}/>
+                        <IconWithIndex index={idx} name='dropdown' onClick={this.toggleAccordionContent}/>
+                      </AccordionTitleCheckbox>
+                      <Accordion.Content active={this.state.activePanes.indexOf(idx) > -1}>
+                        <div style={{'display': 'flex', 'flexDirection': 'column', 'justifyContent': 'space-around'}}>
+                          {s.feeds.map((f) => <Checkbox label={f}/>)}
+                        </div>
+                      </Accordion.Content>
+                    </Menu.Item>
+                  )
+                })
+              }
             </Accordion>
         </Grid.Column>
         <Grid.Column className='mgn-0 pd-0' width={13}>
---
+++ src/api/mockSatellites.js
@@ -0,0 +1,84 @@
+const satellites = [
+  {
+    name: 'Sat1',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat2',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat3',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat4',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat5',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat6',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat7',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat8',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat9',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  },
+  {
+    name: 'Sat10',
+    feeds: [
+      'Ant1',
+      'Ant2',
+      'Ant3'
+    ]
+  }
+]
+
+export default satellites;
